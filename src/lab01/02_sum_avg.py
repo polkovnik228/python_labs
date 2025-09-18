2957b4020000e3f2242c2c30afb22fa193543914
Семён Савинков <semensavinkov@MacBook-Pro-Semen.local>
2025-09-18 19:19:27 +0300

    commit

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,7 @@
+a = float(input("a: ").replace(",", "."))
+b = float(input("b: ").replace(",", "."))
+
+s = a + b
+avg = s / 2
+
+print(f"sum={s:.2f}; avg={avg:.2f}")
---
+++ src/lab01/04_minutes_to_hhmm.py
@@ -0,0 +1,8 @@
+def minutes_to_hhmm(m):
+    hours = m // 60
+    remaining_minutes = m % 60
+    return f"{hours}:{remaining_minutes:02d}"
+
+m = int(input("Минуты: "))
+result = minutes_to_hhmm(m)
+print(f"{result}")
