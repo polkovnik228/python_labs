6d7e0e32c71a30013fa103edf48be474a3f5d1d9
Семён Савинков <semensavinkov@MacBook-Pro-Semen.local>
2025-11-20 12:05:31 +0300

    final lab06

--- src/lab06/cli_text.py
+++ src/lab06/cli_text.py
@@ -48,6 +48,7 @@ def build_parser():
     cat_p.add_argument("-n", action="store_true", help="Нумеровать строки")
     cat_p.set_defaults(func=cmd_cat)
 
+
     stats_p = subparsers.add_parser("stats", help="Частотный анализ слов")
     stats_p.add_argument("--input", required=True, help="Путь к текстовому файлу")
     stats_p.add_argument("--top", type=int, default=5, help="Сколько слов выводить (по умолчанию 5)")
